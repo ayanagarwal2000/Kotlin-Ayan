443cd5318897c35ed4af031554796a00d38f8490
ayanagarwal2000 <56232928+ayanagarwal2000@users.noreply.github.com>
2020-12-06 17:58:04 +0530

    Update datatype.kt

--- kotlin/basics/datatype.kt
+++ kotlin/basics/datatype.kt
@@ -1,8 +1,13 @@
+
+
 fun main(){
+    var f:String?=null
+    val size: Int = f?.length ?: 0
+
     val a:String="Ayan"
     val b:Int=10
     val c:Boolean=true
     val d:Double=10.2
 
-    println(a)
+    println(size)
 }
