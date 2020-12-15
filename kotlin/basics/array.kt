275627932bfeaacf7d91549125fb7bcde0c02d53
ayanagarwal2000 <56232928+ayanagarwal2000@users.noreply.github.com>
2020-12-15 13:36:52 +0530

    Update array.kt

--- kotlin/basics/array.kt
+++ kotlin/basics/array.kt
@@ -1,5 +1,9 @@
 var names= listOf<String>("Hello","World", "this is", "Ayan")
+var mutablenames= mutableListOf<String>("Hello","World", "this is", "Ayan")
+var arraynames= arrayListOf<String>("Hello","World", "this is", "Ayan")
+var mapofnames= mapOf<String,String>("Name" to "Ayan", "Nationality" to "Indian")
 
 fun main(){
     
+
 }
