57a95ca4ff14201c3b6d403619c0d80038f09e8c
ayanagarwal2000 <56232928+ayanagarwal2000@users.noreply.github.com>
2021-01-29 23:11:31 +0530

    Update special.kt

--- kotlin/basics/special.kt
+++ kotlin/basics/special.kt
@@ -3,6 +3,11 @@ fun main(){
     var any:Any="Ayan"
 
     var name:String= any as String
-
     print(name)
+    println("")
+    if(any is String){
+        var len= name.length
+        print(len)
+
+    }
 }
