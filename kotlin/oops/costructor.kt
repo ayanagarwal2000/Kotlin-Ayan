ea276bd44e082e698a354dd3ad406f061b51e348
ayanagarwal2000 <56232928+ayanagarwal2000@users.noreply.github.com>
2021-02-01 17:25:06 +0530

    Update costructor.kt

--- kotlin/oops/costructor.kt
+++ kotlin/oops/costructor.kt
@@ -1,31 +1,31 @@
 fun main(){
     val account=Account(1234,"Ayan",10000f)
-
-    //account.insert(1234,"Ayan",10000f)
+​
+    //account.insert(1234,"ayan",10000f)
     account.deposit(1000f)
     account.withDraw(100f)
     println(account.amount)
     println(account.name)
-
+​
 }
-
+​
 class Account(accNo:Int, name:String, amount:Float){
     var accNo:Int 
     var name:String
     var amount:Float
-
+​
     //Constructor for the respective Class
     init{
         this.accNo=accNo
-        this.name=name
+        this.name=name.capitalize()
         this.amount=amount
     }
-
+​
     fun deposit(money:Float){
         amount += money
         println(amount)
     }
-
+​
     fun withDraw(money:Float){
         if (amount<money){
             println("Not Sufficient Funds")
